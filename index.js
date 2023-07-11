fbd0a27e1ca080c42a26d3b84b09f44c32448b42
GABRIEL ARAÚJO <10160159@lab-412-33.RESTINGA>
2023-07-11 08:41:08 -0300

    sneaker house

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/visual'));
+app.listen(3000, function(){
+  console.log("Servidor no ar - Porta: 3000!")
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/visual/index.html');
+});
+
+app.get('/beneficios', function(req, res){
+    res.sendFile(__dirname + '/visual/beneficios.html');
+});
+
