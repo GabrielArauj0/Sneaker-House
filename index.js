a0f914e9d4d7df8723ce13675024472814bea902
4RAUJ00 <10160159@restinga.ifrs.edu.br>
2023-07-11 20:29:46 -0300

    11/07 atualizado

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('views', __dirname + '/public/visual')
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/visual'));
 
 app.listen(3000, function(){
   console.log("Servidor no ar - Porta: 3000!")
@@ -13,14 +11,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/public/visual/index.html');
+	res.sendFile(__dirname + '/visual/index.html');
 });
 
 app.get('/beneficios', function(req, res){
-    res.sendFile(__dirname + '/public/visual/beneficios.html');
+    res.sendFile(__dirname + '/visual/beneficios.html');
 });
 
 app.get('/vans', function(req, res){
-  res.render('vans.ejs');
+    res.sendFile(__dirname + '/visual/vans.html');
 });
 
