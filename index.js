25c2de97865cf1b2c5404697c7e95f1fffacbe34
GABRIEL ARAÚJO <10160159@lab-412-33.RESTINGA>
2023-07-11 09:50:13 -0300

    'att

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/visual'));
+app.set('views', __dirname + '/public/visual')
+
+app.use(express.static(__dirname + '/public'));
+
 app.listen(3000, function(){
   console.log("Servidor no ar - Porta: 3000!")
 });
@@ -10,10 +13,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/visual/index.html');
+	res.sendFile(__dirname + '/public/visual/index.html');
 });
 
 app.get('/beneficios', function(req, res){
-    res.sendFile(__dirname + '/visual/beneficios.html');
+    res.sendFile(__dirname + '/public/visual/beneficios.html');
+});
+
+app.get('/vans', function(req, res){
+  res.render('vans.ejs');
 });
 
